8c5028c617b5ae22dda0dec64ed9b5744c3427b7
hiro98 <hiro@protagon.space>
2019-10-04 19:53:02 +0200

    log full cache drop

--- src/discourse.lisp
+++ src/discourse.lisp
@@ -39,7 +39,6 @@
   (switch (event :test #'(lambda (item clauses)
                            (member item clauses :test #'string=)))
     ('("post_created" "post_edited" "post_destroyed")
-      (log:info body)
       (let* ((post (agets body "post"))
              (id (agets post "topic_id")))
         (remhash (topic-url id) *cache*)
@@ -53,6 +52,7 @@
     ('("topic_created" "topic_destroyed")
       nil)
     (t
+     (log:info "Full cache drop.")
      (setf *cache* (make-hash-table :test 'equal))))
   *cache*)
 
