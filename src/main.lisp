82dddfc65373a8b9c074cae73003f8341ab47ce7
hiro98 <hiro@protagon.space>
2019-10-05 17:06:54 +0200

    and remove it again

--- src/main.lisp
+++ src/main.lisp
@@ -81,7 +81,6 @@
       (:meta :charset "utf-8")
       (:meta :name "viewport" :content "width=device-width, initial-scale=1.0")
       (:meta :name "robots" :content "index, follow")
-      (:link :rel "canonical" :href (get-config :page-url))
       (:title (str #?"Klausur Nougat - ${,title}"))
       (loop for style in (concatenate 'list ,stylesheets (stylesheets *app*))
             collect
