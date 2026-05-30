cc50b6eb7cbae54941998d9190cd6d41da026794
Valentin Boettcher <hiro@protagon.space>
2026-05-30 19:11:51 -0400

    fix recent api change

--- src/main.lisp
+++ src/main.lisp
@@ -453,7 +453,7 @@
                                                                             (str "⇩"))
                                                                            (dolist (tag tags)
                                                                              (htm
-                                                                              (:mark :class "tag" (str (string-upcase tag))))))
+                                                                              (:mark :class "tag" (str (string-upcase (aget tag :name)))))))
                                                                           (:td :data-label "Jahr" (str year))
                                                                           (:td :data-label "Dozent" (str prof))
                                                                           (:td :data-label "Bemerkungen" (str notes))
